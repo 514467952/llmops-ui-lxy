13b14a22e66d57a4605718cf4f5f13623e0d2b14
liuyuchen <liuyuchen3@xiaohongshu.com>
2026-06-24 17:10:59 +0800

    Pinia实现多页面共享数据状态，就是个单例管理数据

--- src/views/space/apps/ListView.vue
+++ src/views/space/apps/ListView.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter.ts'
+
+const counterStore = useCounterStore()
+</script>
 
 <template>
   <p>这里是AI应用列表</p>
+  <p>数据库里的值是{{ counterStore.count }}</p>
+  <router-link to="/">跳转到主页</router-link>
 </template>
 
 <style scoped></style>
