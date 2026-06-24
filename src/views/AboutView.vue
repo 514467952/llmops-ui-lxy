854573adf6e99c88726bc56b90abae38d37598dc
liuyuchen <liuyuchen3@xiaohongshu.com>
2026-06-24 15:46:13 +0800

    集成TailwindCSS和ArcoDesign

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts"></script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1 class="text-3xl font-bold underline text-blue-700">Hello World!</h1>
+    <p>程序员得梦工厂</p>
+    <a-button type="primary">注册登陆</a-button>
   </div>
 </template>
 
